6c8aa2963bf4caf4e143ab487ff9acb8d5f43ca0
vapaus <snaqerou@gmail.com>
2021-05-05 09:48:06 +0200

    update test and readme

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 hasura where params - create where clause easy
 
 ```javascript
+const totalGross = 2000;
+
 const input = whereClause(
-  whereFilter("fiscalNumber._ilike", "query"),
-  whereFilter("_or.dailyReportNumber._ilike", "query"),
+  whereFilter("fiscalNumber._eq", "order"),
+  whereFilter("_or.dailyReportNumber._in", ["user1", "user2", "user3"]),
   whereFilter("_or.printoutNumber._ilike", "query"),
-  whereFilter("_or._and.systemNumber._ilike", "query")
+  whereFilter("_or._and.systemNumber._ilike", "query"),
+  whereFilter("totalGross._gte", totalGross, totalGross > 3000)
 );
 
 const outputResult = {
-  fiscalNumber: "query",
+  fiscalNumber: {
+    _eq: "order",
+  },
   _or: {
     dailyReportNumber: {
-      _ilike: "query",
+      _in: ["user1", "user2", "user3"],
     },
     printoutNumber: {
       _ilike: "query",
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,18 +2,23 @@ import { whereClause, whereFilter } from ".";
 
 describe("whereClause with whereFilter", () => {
   it("should return correct structure", () => {
+    const totalGross = 2000;
+
     const input = whereClause(
-      whereFilter("fiscalNumber._ilike", "query"),
-      whereFilter("_or.dailyReportNumber._ilike", "query"),
+      whereFilter("fiscalNumber._eq", "order"),
+      whereFilter("_or.dailyReportNumber._in", ["user1", "user2", "user3"]),
       whereFilter("_or.printoutNumber._ilike", "query"),
-      whereFilter("_or._and.systemNumber._ilike", "query")
+      whereFilter("_or._and.systemNumber._ilike", "query"),
+      whereFilter("totalGross._gte", totalGross, totalGross > 3000)
     );
 
     const outputResult = {
-      fiscalNumber: "query",
+      fiscalNumber: {
+        _eq: "order",
+      },
       _or: {
         dailyReportNumber: {
-          _ilike: "query",
+          _in: ["user1", "user2", "user3"],
         },
         printoutNumber: {
           _ilike: "query",
